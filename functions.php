260096f07ef4afe5884c63ad16c09171baba373b
Muhammad Shahid Khan Afridi <muhammadshahidkhanafridi@gmail.com>
2020-08-24 12:14:34 +0500

    function.php
    just commiting test function

--- functions.php
+++ functions.php
@@ -147,11 +147,18 @@ add_action('init','my_first_taxonomy');
 
 //Adding LoginIn and LogOut link in Nav Menu
 add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
-function add_login_logout_link($items, $args) {
+
+function add_login_logout_link($items, $args) 
+{
         ob_start();
         wp_loginout('index.php');
         $loginoutlink = ob_get_contents();
         ob_end_clean();
         $items .= '<li>'. $loginoutlink .'</li>';
     return $items;
+}
+
+function myFntoTestGit()
+{
+	return 1+1;
 }
